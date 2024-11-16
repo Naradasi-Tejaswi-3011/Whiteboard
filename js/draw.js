962192065688be1a01d619a6a3b55186039516fc
Tejaswi <tejaswi.naradasi@gmail.com>
2024-11-16 12:01:41 +0530

    Fix touch events for drawing on phones

--- js/draw.js
+++ js/draw.js
@@ -1,90 +1,70 @@
-let isMouseDown = false;
+let isDrawing = false;
 
-board.addEventListener("mousedown", function(e) {
+// Mouse events
+board.addEventListener("mousedown", function (e) {
+  startDrawing(e.clientX, e.clientY);
+});
+
+board.addEventListener("mousemove", function (e) {
+  if (isDrawing) draw(e.clientX, e.clientY);
+});
+
+board.addEventListener("mouseup", function () {
+  stopDrawing();
+});
+
+// Touch events
+board.addEventListener("touchstart", function (e) {
+  const touch = e.touches[0]; // Get the first touch point
+  startDrawing(touch.clientX, touch.clientY);
+});
+
+board.addEventListener("touchmove", function (e) {
+  const touch = e.touches[0];
+  if (isDrawing) draw(touch.clientX, touch.clientY);
+  e.preventDefault(); // Prevent scrolling while drawing
+});
+
+board.addEventListener("touchend", function () {
+  stopDrawing();
+});
+
+// Drawing functions
+function startDrawing(x, y) {
+  const top = getLocation();
   ctx.beginPath();
-  let top = getLocation();
-  ctx.moveTo(e.clientX, e.clientY - top);
-  isMouseDown = true;
+  ctx.moveTo(x, y - top);
+  isDrawing = true;
 
-  let point = {
-    x: e.clientX,
-    y: e.clientY - top,
+  const point = {
+    x,
+    y: y - top,
     identifier: "mousedown",
     color: ctx.strokeStyle,
-    width: ctx.lineWidth
+    width: ctx.lineWidth,
   };
 
   undoStack.push(point);
-
   socket.emit("mousedown", point);
-  // event emit
-});
-// mmousedown x,y beginPath,moveTo(x,y),color,size
-// mouseMove=> x1,y1, lineTo,stroke
-board.addEventListener("mousemove", function(e) {
-  if (isMouseDown == true) {
-    // console.log(ctx);
-    let top = getLocation();
-
-    ctx.lineTo(e.clientX, e.clientY - top);
-    ctx.stroke();
-    let point = {
-      x: e.clientX,
-      y: e.clientY - top,
-      identifier: "mousemove",
-      color: ctx.strokeStyle,
-      width: ctx.lineWidth
-    };
-    undoStack.push(point);
-    socket.emit("mousemove", point);
-  }
-});
-
-board.addEventListener("mouseup", function(e) {
-  isMouseDown = false;
-});
-
-const undo = document.querySelector(".undo");
-const redo = document.querySelector(".redo");
-
-let interval = null;
-
-undo.addEventListener("mousedown", function() {
-  interval = setInterval(function() {
-    if (undoMaker()) socket.emit("undo");
-  }, 50);
-});
-
-undo.addEventListener("mouseup", function() {
-  clearInterval(interval);
-});
-redo.addEventListener("mousedown", function() {
-  interval = setInterval(function() {
-    if (redoMaker()) socket.emit("redo");
-  }, 50);
-});
-redo.addEventListener("mouseup", function() {
-  clearInterval(interval);
-});
-
-function redraw() {
-  ctx.clearRect(0, 0, board.width, board.height);
-
-  for (let i = 0; i < undoStack.length; i++) {
-    let { x, y, identifier, color, width } = undoStack[i];
-    ctx.strokeStyle = color;
-    ctx.lineWidth = width;
-    if (identifier == "mousedown") {
-      ctx.beginPath();
-      ctx.moveTo(x, y);
-    } else if (identifier == "mousemove") {
-      ctx.lineTo(x, y);
-      ctx.stroke();
-    }
-  }
 }
 
-function getLocation() {
-  const { top } = board.getBoundingClientRect();
-  return top;
+function draw(x, y) {
+  const top = getLocation();
+  ctx.lineTo(x, y - top);
+  ctx.stroke();
+
+  const point = {
+    x,
+    y: y - top,
+    identifier: "mousemove",
+    color: ctx.strokeStyle,
+    width: ctx.lineWidth,
+  };
+
+  undoStack.push(point);
+  socket.emit("mousemove", point);
+}
+
+function stopDrawing() {
+  isDrawing = false;
 }
